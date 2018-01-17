8555163c3c0ac54fd064b580c776a03b3e17ec50
Ricardo Morales Paredes <pntra2.team@gmail.com>
2018-01-17 11:23:35 -0600

    adding plugins to generate a functional jar

--- src/main/java/com/alchemistscode/commands/OpusMagnum.java
+++ src/main/java/com/alchemistscode/commands/OpusMagnum.java
@@ -1,5 +1,16 @@
 package com.alchemistscode.commands;
 
-public class OpusMagnum {
+import static picocli.CommandLine.*;
 
+@Command(name="opus", header="Opus Magnum")
+public class OpusMagnum implements Runnable{
+
+	@Option(names = {"-v", "--version"}, versionHelp = true, description = "display version info")
+	boolean versionInfoRequested;
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
